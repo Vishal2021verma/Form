14894ed2b08c52b2d26288d885b10d1498fe6eaa
Vishal Verma <black2020panther@gmail.com>
2022-04-21 07:36:59 +0530

    add maxlength and test the bodyParser

--- app.js
+++ app.js
@@ -7,11 +7,22 @@
 //     "mongodb": "^4.5.0"
 //   }
 const express = require('express');
+const bodyParser = require('body-parser');
+
+
 
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", (req,res)=>{
     res.sendFile(__dirname + "/from.html");
+});
+app.post("/", (req,res) => {
+    const formData = {
+        name : req.body.fname
+    }
+    console.log(formData.name);
+    res.end();
 });
 
 app.listen(8080, () => {
